6e785967d6ece81235e2a1fb0e48b73f0cb6fa8d
ingeducon <ingeducon@gmail.com>
2022-03-03 00:52:29 -0500

    Implementacion de creacion

--- functions/dogos/dogos.ts
+++ functions/dogos/dogos.ts
@@ -1,10 +1,14 @@
 import { Handler } from "@netlify/functions";
 import { readDogos } from "./readDogos";
+import { createDogos } from "./createDogos";
 
 const handler: Handler = (event, context, callback) => {
   switch (event.httpMethod) {
     case "GET":
       return readDogos(event, context, callback);
+      break;
+    case "POST":
+      return createDogos(event, context, callback);
       break;
   }
 
